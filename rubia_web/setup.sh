ec6e671f2f39367dd289b7a46a980443227a0375
lazaropd <lazaro.domiciano@gmail.com>
2019-11-02 15:58:21 -0300

    criação dos arquivos de setup para Heroku

---
+++ rubia_web/setup.sh
@@ -0,0 +1,14 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = lazaro.domiciano@gmail.com\"\n\
+" >
+~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
